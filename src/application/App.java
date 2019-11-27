1305a1a81d70bd1ac3a64a055de2fe5a2636e15a
Carla Vieira <carlaamvieira@gmail.com>
2019-11-27 10:40:31 -0300

    final

--- src/application/App.java
+++ src/application/App.java
@@ -6,21 +6,22 @@ import util.Teclado;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("\n###                             Mercadinho do Raul                      ###");
+        System.out.println("\n###                                Mercadinho do Raul                         ###");
         menu();
     }
 
     private static void menu(){
         int op;
         System.out.println("\n       ===================================================================");
-        System.out.println("      | Realizar Troca Por:                                               |");
-        System.out.println("      |    1. Prateleira preferencial do produto                          |");
-        System.out.println("      |    2. Prateleira que está com um produto a mais tempo (FIFO)      |");
-        System.out.println("      |    3. Prateleira que está com produto menos recentemente comprado |");
-        System.out.println("      |    4. Prateleira que está com um produto de lote menos distante   |");
-        System.out.println("      |    5. Prateleira que está com um produto menos vendido até então  |");
-        System.out.println("      |    6. Relatório final                                             |");
-        System.out.println("      |    0. Sair                                                        |");
+        System.out.println("      | Realizar Troca Por:                                                     |");
+        System.out.println("      |    1. Prateleira preferencial do produto                                |");
+        System.out.println("      |    2. Prateleira que está com um produto a mais tempo (FIFO)            |");
+        System.out.println("      |    3. Prateleira que está com produto menos recentemente comprado       |");
+        System.out.println("      |    4. Prateleira que está com um produto de lote menos distante         |");
+        System.out.println("      |    5. Prateleira que está com produto menos recentemente comprado (NRU) |");
+        System.out.println("      |    6. Prateleira que está com produto menos recentemente comprado (Env) |");
+        System.out.println("      |    7. Relatório final                                                   |");
+        System.out.println("      |    0. Sair                                                              |");
         System.out.println("       ===================================================================\n");
 
         System.out.print("Informe uma opcão: ");
@@ -48,6 +49,10 @@ public class App {
                 menu();
                 break;
             case 6:
+                new Market().changeByAging();;
+                menu();
+                break;
+            case 7:
                 System.out.println("**** Relatório Final *****");
                 System.out.println("\nTroca por: Prateleira preferencial do produto");
                 new Market().changeByShelfPreference();
@@ -59,9 +64,9 @@ public class App {
                 new Market().changeByShelfDistance();
                 System.out.println("\nTroca por: Prateleira que está com um produto menos vendido até então");
                 new Market().changeByLessSold();
-                System.out.println("\nTroca por: Prateleira com produto que não foi vendido recentemente (NRU)");
+                System.out.println("\nTroca por: Prateleira com produto menos recentemente comprado (NRU)");
                 new Market().changeByNRU();
-                System.out.println("\nTroca por: Prateleira com produto que não foi vendido recentemente (Envelhecimento)");
+                System.out.println("\nTroca por: Prateleira com produto menos recentemente comprado (Envelhecimento)");
                 new Market().changeByAging();
                 System.out.println();
                 menu();
